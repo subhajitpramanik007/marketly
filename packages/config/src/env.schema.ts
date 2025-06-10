dd75fbc15ba4561ba0695c9823af17c94d8115cb
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-06-10 12:12:37 +0530

    feat(config): add configuration files and environment schema validation

---
+++ packages/config/src/env.schema.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  DATABASE_URL: z.string().url(),
+
+  // Cors origin string -> array
+  CORS_ORIGIN: z
+    .string()
+    .transform(s => s.split(','))
+    .array(),
+
+  // Auth
+  JWT_SECRET: z.string().min(10),
+  JWT_EXPIRES_IN: z.string().default('15m'),
+  REFRESH_TOKEN_SECRET: z.string().min(10),
+  REFRESH_TOKEN_EXPIRES_IN: z.string().default('7d'),
+
+  // Redis
+  REDIS_URL: z.string().url(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.string().transform(Number),
+  REDIS_PASSWORD: z.string(),
+
+  // SMTP
+  SMTP_HOST: z.string(),
+  SMTP_PORT: z.string().transform(Number),
+  SMTP_SERVICE: z.string(),
+  SMTP_USER: z.string(),
+  SMTP_PASSWORD: z.string(),
+  SMTP_FROM: z.string(),
+
+  //   Service
+  API_URL: z.string().url().default('http://localhost:8080'),
+  CLIENT_URL: z.string().url().default('http://localhost:3000'),
+});
---
+++ packages/config/src/env.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import { config as loadEnv } from 'dotenv';
+import { envSchema } from './env.schema';
+
+loadEnv({ path: path.resolve(__dirname, "../../../.env") });
+
+try {
+  loadEnv({ path: path.resolve(process.cwd(), '.env') });
+} catch {}
+
+const parsed = envSchema.safeParse(process.env);
+
+if (!parsed.success) {
+  console.error('❌ Invalid environment variables:', parsed.error.format());
+  process.exit(1);
+}
+
+export const env = parsed.data;
+
+export const isDev = env.NODE_ENV === 'development';
